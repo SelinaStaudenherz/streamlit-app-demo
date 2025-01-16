cd144d245ed7aefba20d32d10161c72a7e424b8d
SelinaStaudenherz <sstaudenherz2020@fau.edu>
2025-01-16 13:22:06 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,3 @@
 import streamlit as st
 st.title("My Streamlit App")
-st.write("Welcome to my first Streamlit app deployed on Streamlit Cloud!")
+st.write("This is an updated version of my Streamlit App")
